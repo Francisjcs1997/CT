0704283a43b88653af965f867de8de8de422eaff
Francisjcs1997 <86250781+Francisjcs1997@users.noreply.github.com>
2021-06-22 19:39:00 +0100

    Update ALS.R

--- ALS.R
+++ ALS.R
@@ -1,5 +1,5 @@
 #Depois do tratamento de dados realizado e calculo da CO_I:
-c1=2;c2=2;c3=2 #número P,Q,R, respetivamente
+c1=2;c2=2;c3=2 #nÃºmero P,Q,R, respetivamente
 
 Xa=as.matrix(cbind(CO_I[1:4,],CO_I[5:8,],CO_I[9:12,]))
 Xb=t(as.matrix(CO_I))
@@ -7,11 +7,11 @@ l1=as.numeric(t(CO_I[1:4,]));l2=as.numeric(t(CO_I[5:8,]));l3=as.numeric(t(CO_I[9
 Xc=rbind(l1,l2,l3)
 
 ########################
-#Inicialização 1 com coeficientes nulos
+#InicializaÃ§Ã£o 1 com coeficientes nulos
 b=matrix(0,nrow=24,ncol=c2)
 c=matrix(0,nrow=3,ncol=c3)
 
-#Inicialização 2 com Wb Wb^T
+#InicializaÃ§Ã£o 2 com Wb Wb^T
 decomp_Xb=svd(Xb%*%t(Xb))
 b=as.matrix(decomp_Xb$u[,1:c2])
 decomp_Xc=svd(Xc%*%t(Xc))
@@ -54,14 +54,10 @@ while(erro==FALSE){
 
 G=array(t(A)%*%Xa%*%(C%x%B),dim=c(c1,c2,c3))
 perc=sum(G^2)/sum(Xa^2)
-G_prop=(G^2/sum(G^2))*perc*100
-rownames(A)=colnames(saliva1)
-rownames(B)=colnames(urina1)
-#C=as.matrix(C,nrow=3,ncol=1)
-rownames(C)=c("1T","2T","3T")
 
 
-#ALS com função Tucker do R
+
+#ALS com funÃ§Ã£o Tucker do R
 library(CA3variants)
 
 CO_I=as.matrix(CO_I)
@@ -69,8 +65,5 @@ W=array(c(CO_I[1:4,],CO_I[5:8,],CO_I[9:12,]),dim=c(4,24,3))
 U=tucker(W,c1,c2,c3)
 
 A=U$a;B=U$b;C=U$cc
-rownames(A)=colnames(saliva1)
-rownames(B)=colnames(urina1)
-rownames(C)=c("1T","2T","3T")
 G=array(U$g,dim=c(c1,c2,c3))
-sum(G^2)/sum(Xa^2)
+perc=sum(G^2)/sum(Xa^2)
