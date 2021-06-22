b58e7df523882681c2afe96cb808ced1451ca7b6
Francisjcs1997 <86250781+Francisjcs1997@users.noreply.github.com>
2021-06-22 23:54:45 +0100

    Update CoTucker.R

--- CoTucker.R
+++ CoTucker.R
@@ -1,83 +1,45 @@
-library(CA3variants)
 #####
 #TRATAMENTO DE DADOS
+#(Dados nÃ£o disponibilizados)
 
-#Extração de dados
-saliva=read.csv2("saliva.csv")
-rownames(saliva)=saliva$ï..code
-saliva=saliva[,-1]
-colnames(saliva)=c("etanol","NAG","acetoina","ureia")
-
-urina=read.csv2("urina.csv")
-rownames(urina)=urina$ï..code
-urina=urina[,-1]
-colnames(urina)=c("Pn3G","P3G","X4.DEA","X3.HIBA","alanina","X4.DTA","colina","carnitina",
-                  "glicina","creatina","creatinina","glicose","hipurico","GAA","X1.6.anidro",
-                  "treonina","trigonelina","histidina","taurina","X2.KG","U3","U1","N5AC","U2")
-
-#Normalização
-saliva1=scale(saliva[1:7,],center=T,scale=F)
-saliva2=scale(saliva[8:14,],center=T,scale=F)
-saliva3=scale(saliva[15:21,],center=T,scale=F)
+#Centrar cubo X e normalizaÃ§Ã£o do cubo Y
+saliva1=scale(saliva[1:7,],center=T,scale=F) #X1
+saliva2=scale(saliva[8:14,],center=T,scale=F) #X2
+saliva3=scale(saliva[15:21,],center=T,scale=F) #X3
 saliva_norm=rbind(saliva1,saliva2,saliva3)
 saliva_norm=data.frame(saliva_norm)
 
-urina1=scale(urina[1:7,],center=T,scale=T)
-urina2=scale(urina[8:14,],center=T,scale=T)
-urina3=scale(urina[15:21,],center=T,scale=T)
+urina1=scale(urina[1:7,],center=T,scale=T) #Y1
+urina2=scale(urina[8:14,],center=T,scale=T) #Y2
+urina3=scale(urina[15:21,],center=T,scale=T) #Y3
 urina_norm=rbind(urina1,urina2,urina3)
 urina_norm=data.frame(urina_norm)
-
-#remove(saliva,urina)
-
 
 #####
 #CO-INERCIA
 
 Dn=as.matrix(diag(1/7,nrow=7,ncol=7))
-Z1=t(saliva1)%*%Dn%*%urina1
-Z2=t(saliva2)%*%Dn%*%urina2
-Z3=t(saliva3)%*%Dn%*%urina3
+Z1=t(saliva1)%*%Dn%*%urina1 #W1
+Z2=t(saliva2)%*%Dn%*%urina2 #W2
+Z3=t(saliva3)%*%Dn%*%urina3 #W3
 
 CO_I=rbind(Z1,Z2,Z3)
 CO_I=data.frame(CO_I)
 
-
+#Calculo co-inÃ©rcia
 A=sum(diag(saliva1%*%t(saliva1)%*%Dn%*%urina1%*%t(urina1)%*%Dn))
 B=sum(diag(saliva2%*%t(saliva2)%*%Dn%*%urina2%*%t(urina2)%*%Dn))
 C=sum(diag(saliva3%*%t(saliva3)%*%Dn%*%urina3%*%t(urina3)%*%Dn))
-barplot(c(A,B,C),names.arg = c("1ºT","2ºT","3ºT"),ylab="Interação")
-
-
-
-#####
-#ESCOLHA E CONSTRUÇÃO DO MODELO
-
-#1º Modo
-Xa=as.matrix(cbind(CO_I[1:4,],CO_I[5:8,],CO_I[9:12,]))
-
-#2º Modo
-Xb=t(CO_I)
-
-#3º Modo
-l1=as.numeric(t(CO_I[1:4,]));l2=as.numeric(t(CO_I[5:8,]));
-l3=as.numeric(t(CO_I[9:12,]))
-Xc=rbind(l1,l2,l3)
-
-
-U=tucker(array(Xa,dim=c(4,24,3)),2,2,2)
-A=U$a;B=U$b;C=U$cc
-rownames(A)=colnames(saliva1)
-rownames(B)=colnames(urina1)
-rownames(C)=c("1T","2T","3T")
-G=array(U$g,dim=c(2,2,2))
-perc=sum(G^2)/sum(Xa^2)
+barplot(c(A,B,C),names.arg = c("1ÂºT","2ÂºT","3ÂºT"),ylab="InteraÃ§Ã£o")
 
 
 #####
-#VISUALIZAÇÃO DE COMPONENTES
+#ESCOLHA E CONSTRUÃ‡ÃƒO DO MODELO ATRAVÃ‰S DO ALS EM 
+
+#####
+#VISUALIZAÃ‡ÃƒO DE COMPONENTES
 par(mfrow=c(1,3))
-#1º Modo
+#1Âº Modo
 plot(NA,xlim=c(min(A[,1],0),max(A[,1],0)),ylim=c(min(A[,2]-0.1,0),max(A[,2]+0.1,0)),
      xlab="P1",ylab="P2",main="P")
 abline(v=0, col='black', lty=10, lwd=0.3)
@@ -88,7 +50,7 @@ text(A[2,1]+0.08,A[2,2]+0.06,labels = rownames(A)[2],cex=2)#NAG
 text(A[3,1]-0.18,A[3,2],labels = rownames(A)[3],cex=2)#acetoine
 text(A[4,1]-0.05,A[4,2]+0.05,labels = rownames(A)[4],cex=2)#Urea
 
-#2ª Modo
+#2Âª Modo
 plot(NA,xlim=c(min(B[,1]-0.1,0),max(B[,1],0)),ylim=c(min(B[,2],0),max(B[,2],0)),
      xlab="Q1",ylab="Q2",
      main="Q",col="red")
@@ -96,7 +58,7 @@ abline(v=0, col='black', lty=10, lwd=0.3)
 abline(h=0, col='black', lty=10, lwd=0.3)
 text(B[,1],B[,2],labels = rownames(B),cex=2, pos=3,col="red")
 
-#3º Modo
+#3Âº Modo
 plot(C[,1],C[,2],xlim=c(min(C[,1],0),max(C[,1],0)),ylim=c(min(C[,2],0),max(C[,2],0)),
      xlab="R1",ylab="R2",
      main="R",col="blue")
@@ -106,9 +68,9 @@ text(C[1,1],C[1,2]+0.05,labels = rownames(C)[1],cex=2,col="blue")#1T
 text(C[2,1],C[2,2]+0.05,labels = rownames(C)[2],cex=2,col="blue")#2T
 text(C[3,1],C[3,2]-0.05,labels = rownames(C)[3],cex=2,col="blue")#3T
 par(mfrow=c(1,1))
-#####
-#CONSTRUÇÃO DOS BIPLOTS
 
+#####
+#CONSTRUÃ‡ÃƒO DOS BIPLOTS
 #R1
 G1=svd(G[,,1])
 Ax1=((4/24)^(1/4))*A%*%G1$u%*%diag(sqrt(G1$d))
@@ -134,7 +96,7 @@ Bx2=((24/2)^(1/4))*B%*%G2$v%*%diag(sqrt(G2$d))
 xmin=min(Ax2[,1],Bx2[,1]);xmax=max(Ax2[,1],Bx2[,1])
 ymin=min(Ax2[,2],Bx2[,2]);ymax=max(Ax2[,2],Bx2[,2])
 
-#2º componente do 3º modo
+#2Âº componente do 3Âº modo
 plot(NA,col="red",xlim=c(xmin-0.005,xmax),ylim=c(ymin,ymax+0.005),
      main="R2",xlab="Primeira Componente",ylab="Segunda Componente")
 abline(v=0, col='black', lty=10, lwd=0.3)
